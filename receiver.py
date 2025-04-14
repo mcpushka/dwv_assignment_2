08a8caa32fc639ed8119c4b7265cd5d4309c17e9
mcpushka <ssp.prog@mail.ru>
2025-04-15 00:25:47 +0300

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -5,7 +5,6 @@ from collections import deque
 app = Flask(__name__)
 CORS(app)
 
-# Очередь последних N пакетов
 MAX_PACKETS = 1000
 packets = deque(maxlen=MAX_PACKETS)
 
@@ -15,7 +14,6 @@ def receive():
     if not data:
         return jsonify({"error": "Invalid JSON"}), 400
 
-    # Добавляем в очередь
     packets.append(data)
     return jsonify({"status": "received"}), 200
 
